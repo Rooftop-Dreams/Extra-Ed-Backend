b57c296d8db30e344a935c987fd46dfdad465967
Nahom Debele <nahomdebele002@gmail.com>
2024-02-11 22:21:19 +0300

    added the code functionality and working to wards the expected result

--- src/User/Dto/user.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  user_id: number;
-  username: string;
-  email: string;
-  age: number;
-  country: string;
-  grade: number;
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,26 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmConfig } from "./Config/db.config";
-
+import { PaymentModule } from "./payment/payment.module";
+import { UserModule } from "./user/user.module";
+import { BookModule } from "./book/book.module";
+import { CategoryModule } from "./category/category.module";
+import { ScheduleModule } from "./schedule/schedule.module";
+import { NoteModule } from "./note/note.module";
+import { ChatModule } from "./chat/chat.module";
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       useFactory: typeOrmConfig,
     }),
+    PaymentModule,
+    UserModule,
+    BookModule,
+    CategoryModule,
+    ScheduleModule,
+    NoteModule,
+    ChatModule,
   ],
   controllers: [],
   providers: [],
