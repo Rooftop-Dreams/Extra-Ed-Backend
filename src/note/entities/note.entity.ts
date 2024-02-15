b888a87773bbe556cc2193510212d904e0eadd74
Nahom Debele <nahomdebele002@gmail.com>
2024-02-15 23:17:53 +0300

    added the note entity

--- src/note/entities/note.entity.ts
+++ src/note/entities/note.entity.ts
@@ -1,1 +1,35 @@
-export class Note {}
+import { UserEntity } from "src/user/entities/user.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Note {
+    @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  is_available: boolean;
+
+  @Column()
+  category_id: string;
+
+  @Column()
+  grade: number;
+
+  @Column({ nullable: true })
+  cover_image_url: string;
+
+  @Column({ nullable: true })
+  pdf_url: string;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  created_at: Date;
+
+  @OneToMany(() => UserEntity, (user) => user.phone)
+  users: UserEntity[];
+}
